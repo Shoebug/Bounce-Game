7f32779d87bd244945e84df2dca904a7fb1cd4fb
Nathan Roche <nathanroche2@gmail.com>
2023-12-12 14:32:53 +0000

    paddle, ball, block colour change

--- game.py
+++ game.py
@@ -23,13 +23,12 @@ def start_game(event):
         file1.close()
         score.configure(text="Score: 00\n\nHighscore: " + highscore)
         canvas.delete("all")
-        BALL_COLOR = ["red", "yellow", "green"]
+        BALL_COLOR = ["blue", "yellow", "black"]
         BRICK_COLOR = ["PeachPuff3", "dark slate gray", "rosy brown", "light goldenrod yellow", "turquoise3", "salmon",
-                       "light steel blue", "dark khaki", "pale violet red", "orchid", "tan", "MistyRose2",
-                       "DodgerBlue4", "wheat2", "RosyBrown2", "bisque3", "DarkSeaGreen1"]
+                       "light steel blue", "dark khaki"]
         random.shuffle(BALL_COLOR)
         # Colour of the paddle
-        paddle = Paddle(canvas, "blue")
+        paddle = Paddle(canvas, "red")
         bricks = []
         for i in range(0, 5):
             b = []
